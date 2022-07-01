aa0c2d51365b2e6f52c36c39a01578043641b459
Nishad Ahamed <63359153+nish-ahmd@users.noreply.github.com>
2022-07-01 23:07:27 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Footer.css";
+
+
+const Footer = () => {
+  return (
+    <div>
+     
+
+<footer class="footer-distributed">
+
+    <div class="footer-left">
+
+        
+        <p>Company Name &copy; 2015</p>
+    </div>
+
+</footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+import { CSSTransition } from "react-transition-group";
+
+export default function Navbar() {
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
+
+  return (
+    <header className="Header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Android_O_Preview_Logo.png/1024px-Android_O_Preview_Logo.png" className="Logo" alt="logo" />
+      <CSSTransition
+        in={!isSmallScreen || isNavVisible}
+        timeout={350}
+        classNames="NavAnimation"
+        unmountOnExit
+      >
+        <nav className="Nav">
+          
+          <button>Logout</button>
+        </nav>
+      </CSSTransition>
+      <button onClick={toggleNav} className="Burger">
+        🍔
+      </button>
+    </header>
+  );
+}
